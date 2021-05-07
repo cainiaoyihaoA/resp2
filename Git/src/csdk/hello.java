3751d21804ed5b1179ab7fdbca90c0fe09b5f626
zhouyong <zhouyong2018212227@163.com>
2021-05-07 20:48:41 +0800

    提交到本地仓库

---
+++ Git/src/csdk/hello.java
@@ -0,0 +1,15 @@
+package csdk;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+        System.out.println("的人反弹规划局");
+    }
+}
