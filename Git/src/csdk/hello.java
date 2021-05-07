9ed63bbe8c2c142e238e13577bd0d4c7ff944d75
zhouyong <zhouyong2018212227@163.com>
2021-05-07 21:23:56 +0800

    克隆代码提交

--- Git/src/csdk/hello.java
+++ Git/src/csdk/hello.java
@@ -11,8 +11,14 @@ public class hello {
         System.out.println("的人反弹规划局");
         System.out.println("的人反弹规划局");
         System.out.println("的人反弹规划局");
+        hello one =new hello();
+        one.eat();
+        one.run();
     }
     public void eat(){
         System.out.println("吃饭");
+    }
+    public void run(){
+        System.out.println("跑步");
     }
 }
